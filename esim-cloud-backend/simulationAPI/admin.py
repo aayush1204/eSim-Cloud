0c9ca00d7ae5a6c0a926739efbb8323a99f4e521
aayush1204 <aayush.halgekar@gmail.com>
2021-05-28 10:48:39 +0530

    test

--- esim-cloud-backend/simulationAPI/admin.py
+++ esim-cloud-backend/simulationAPI/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from simulationAPI.models import Task, spiceFile
 
 # Register your models here.
+# admin.site.register(Task)
+admin.site.register(spiceFile)
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('task_time', 'task_id')
